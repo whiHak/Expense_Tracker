70dc89883b6954429ad420bf77e123847a4804b6
whiHak <betse1111111111@gmail.com>
2024-11-08 12:13:44 +0300

    added stats page

---
+++ lib/screens/stats/stats.dart
@@ -0,0 +1,35 @@
+import 'package:expense_tracker/screens/stats/chart.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StatScreen extends StatelessWidget {
+  const StatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Transactions",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              // color: Colors.red,
+              child: const MyChart(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
