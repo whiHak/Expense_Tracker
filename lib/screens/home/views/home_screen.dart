ac0c521e865847a283fda7823b7ff4abd8269061
whiHak <betse1111111111@gmail.com>
2024-11-08 12:13:07 +0300

    added bottomNavigationBar

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,28 +1,48 @@
 import 'dart:math';
 
 import 'package:expense_tracker/screens/home/views/main_screen.dart';
+import 'package:expense_tracker/screens/stats/stats.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+  late Color selectedItem = Colors.blue;
+  Color unselectedItem = Colors.grey;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(),
       bottomNavigationBar: ClipRRect(
         borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
         child: BottomNavigationBar(
+          onTap: (value) {
+            setState(() {
+              index = value;
+            });
+          },
           backgroundColor: Colors.white,
           showSelectedLabels: false,
           showUnselectedLabels: false,
           elevation: 3,
-          items: const [
+          items: [
             BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.home), label: "Home"),
+                icon: Icon(
+                  CupertinoIcons.home,
+                  color: index == 0 ? selectedItem : unselectedItem,
+                ),
+                label: "Home"),
             BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.graph_square_fill), label: "Stat")
+                icon: Icon(CupertinoIcons.graph_square_fill,
+                    color: index == 1 ? selectedItem : unselectedItem),
+                label: "Stat")
           ],
         ),
       ),
@@ -44,7 +64,7 @@ class HomeScreen extends StatelessWidget {
               CupertinoIcons.add,
             ),
           )),
-      body: const MainScreen(),
+      body: index == 0 ? const MainScreen() : const StatScreen(),
     );
   }
 }
